cfd94963867532955042790f5efec4710af4d328
omairapalacios <cpalaciosanchez@hotmail.com>
2020-10-29 14:22:30 -0500

    add routes

---
+++ src/routes/index.ts
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+const router = Router();
+
+import { getPhoto, getPhotos, createPhoto, deletePhoto, updatePhoto } from '../controllers/photo_controllers';
+import multer from '../libs/multer';
+
+router.route('/photos')
+  .get(getPhotos)
+  .post(multer.single('image'), createPhoto);
+
+
+router.route('/photos/:id')
+  .get(getPhoto)
+  .put(updatePhoto)
+  .delete(deletePhoto);
+  
+
+
+
+
+export default router;
