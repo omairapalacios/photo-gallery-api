e012d262606fabfc11eadd583426cadc2ccf23d4
omairapalacios <cpalaciosanchez@hotmail.com>
2020-10-29 12:58:54 -0500

    add config mongo db

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,23 @@
 import express from 'express';
+import morgan from 'morgan';
+import path from 'path';
+
+import indexRoutes from './routes/index';
 
 const app = express();
 
 
 // settings
 app.set('port',process.env.PORT || 4000);
+app.use(express.json());
+
+// middlewares
+
+app.use(morgan('dev'));
+app.use('/api', indexRoutes);
+
+// store public file
+
+app.use('/uploads', express.static(path.resolve('uploads')));
 
 export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import app from './app';
+import {startConnection} from './database';
 
 function main() {
+  startConnection();
   app.listen(app.get('port'));
   console.log('Server on port', app.get('port'));
 }
